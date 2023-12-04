c9763d0b8038a12d8ad9cbec9456350462166e34
Rachel <gggggrea@gmail.com>
2023-12-04 12:43:02 +0000

    ✅ Solve day2 part2

--- 2023/day02/solution.py
+++ 2023/day02/solution.py
@@ -3,6 +3,8 @@ import re
 
 lines = read_text_file()
 
+################ Part1 ################
+
 rule = {"red": 12, "green": 13, "blue": 14}
 
 id_sum = 0
@@ -21,3 +23,20 @@ for line in lines:
         id_sum += int(id)
 
 print("Part1 Sum of IDs is: ", id_sum)
+
+################ Part2 ################
+
+power_sum = 0
+for line in lines:
+    [game_id, sets] = line.split(":")
+
+    set_power = 1
+    for colour in ["red", "green", "blue"]:
+        single_colour_values = list(map(int, re.findall(rf"(\d+) {colour}", sets)))
+        max_single_colour_value = max(single_colour_values)
+
+        set_power *= max_single_colour_value
+
+    power_sum += set_power
+
+print("Part2 Sum of Power is: ", power_sum)
