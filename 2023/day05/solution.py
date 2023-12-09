3bae75ea7b6dc20c265fff1e7fb7c4120005f833
Rachel <gggggrea@gmail.com>
2023-12-09 22:13:37 +0000

    ✅ Solve day5 part1

--- 2023/day05/solution.py
+++ 2023/day05/solution.py
@@ -2,57 +2,47 @@ import re
 
 
 def read_text_file(file_path="input.txt"):
-    text_file = open(file_path, "r")
-
-    text = text_file.read()
-
-    text_file.close()
-
-    return text
+    with open(file_path, "r") as text_file:
+        return text_file.read()
 
 
 text = read_text_file()
 
-almanac = [section.strip() for section in text.strip().split("\n\n")]
+almanac = [section.strip().split("\n") for section in text.strip().split("\n\n")]
 
-seeds = re.findall(r"\d+", almanac[0])
+seeds = map(int, re.findall(r"\d+", almanac[0][0]))
+
+
+def get_ranges(map_section):
+    ranges = []
+    for line in map_section[1:]:
+        destination, source, range_len = map(int, re.findall(r"\d+", line))
+
+        ranges.append((destination, source, source + range_len))
+
+    return ranges
+
+
+maps = [get_ranges(section) for section in almanac[1:]]
 
 
 def map_numbers(seed_number):
     destination_number = None
-    source_number = seed_number if destination_number == None else destination_number
-    for map in almanac[1:]:
-        lines = map.split("\n")
+    source_number = seed_number if destination_number is None else destination_number
 
-        destination_numbers = []
-        source_numbers = []
-        for line in lines[1:]:
-            [destination, source, range_len] = re.findall(r"\d+", line)
-
-            for d in range(int(destination), int(destination) + int(range_len)):
-                destination_numbers.append(d)
-
-            for s in range(int(source), int(source) + int(range_len)):
-                source_numbers.append(s)
-
-        if source_number in source_numbers:
-            source_number_index = source_numbers.index(source_number)
-            destination_number = destination_numbers[source_number_index]
+    for map in maps:
+        for destination_start, source_start, source_end in map:
+            if source_start <= source_number < source_end:
+                destination_number = destination_start + (source_number - source_start)
+                break
         else:
             destination_number = source_number
 
         source_number = destination_number
 
-        # print(source_number, destination_number)
-
     return destination_number
 
 
-location_numbers = []
-for seed in seeds:
-    location_number = map_numbers(int(seed))
+location_numbers = [map_numbers(seed) for seed in seeds]
 
-    location_numbers.append(location_number)
-print(location_numbers)
-
-print("Part1 lowest location number is: ", min(location_numbers))
+print("Part 1 lowest location number is:", min(location_numbers))
