21425adf17487c715cb717f183063fdb0742e6ae
Rachel <gggggrea@gmail.com>
2023-12-12 14:06:15 +0000

    Refactor code

--- 2023/day07/solution.py
+++ 2023/day07/solution.py
@@ -15,61 +15,47 @@ lines = read_text_file()
 ################ Part1 ################
 
 
-def is_pattern_matched(counter, pattern):
-    if pattern == sorted(list(counter.values()), reverse=True):
-        return True
-    else:
-        False
+def score_hand_value(hand):
+    label_scores = list(map("_23456789TJQKA".index, hand))
 
-
-def rank_hand_value(hand):
-    label_ranks = list(map("_23456789TJQKA".index, hand))
-
-    new_label_ranks = []
-    for label_rank in label_ranks:
-        if label_rank < 10:
-            new_label_ranks.append("0" + str(label_rank))
+    sorted_label_scores = []
+    for label_score in label_scores:
+        if label_score < 10:
+            sorted_label_scores.append("0" + str(label_score))
         else:
-            new_label_ranks.append(str(label_rank))
+            sorted_label_scores.append(str(label_score))
 
-    return "".join(new_label_ranks)
+    return "".join(sorted_label_scores)
 
 
-ranked_hand = []
+scored_hands = []
 for line in lines:
     hand, bid = line.replace("\n", "").split(" ")
 
-    counter_hand = Counter(hand)
+    hand_type = sorted(list(Counter(hand).values()), reverse=True)
 
-    hand_type = sorted(list(counter_hand.values()), reverse=True)
+    if hand_type == [5]:  # five of a kind
+        scored_hands.append([hand, int(bid), "7" + score_hand_value(hand)])
+    if hand_type == [4, 1]:  # four of a kind
+        scored_hands.append([hand, int(bid), "6" + score_hand_value(hand)])
+    if hand_type == [3, 2]:  # full house
+        scored_hands.append([hand, int(bid), "5" + score_hand_value(hand)])
+    if hand_type == [3, 1, 1]:  # three of a kind
+        scored_hands.append([hand, int(bid), "4" + score_hand_value(hand)])
+    if hand_type == [2, 2, 1]:  # two pair
+        scored_hands.append([hand, int(bid), "3" + score_hand_value(hand)])
+    if hand_type == [2, 1, 1, 1]:  # one pair
+        scored_hands.append([hand, int(bid), "2" + score_hand_value(hand)])
+    if hand_type == [1, 1, 1, 1, 1]:  # high card
+        scored_hands.append([hand, int(bid), "1" + score_hand_value(hand)])
 
-    five_of_a_kind = is_pattern_matched(counter_hand, [5])
-    four_of_a_kind = is_pattern_matched(counter_hand, [4, 1])
-    full_house = is_pattern_matched(counter_hand, [3, 2])
-    three_of_a_kind = is_pattern_matched(counter_hand, [3, 1, 1])
-    two_pair = is_pattern_matched(counter_hand, [2, 2, 1])
-    one_pair = is_pattern_matched(counter_hand, [2, 1, 1, 1])
-    high_card = is_pattern_matched(counter_hand, [1, 1, 1, 1, 1])
-
-    if five_of_a_kind:
-        ranked_hand.append([hand, int(bid), "7" + rank_hand_value(hand)])
-    if four_of_a_kind:
-        ranked_hand.append([hand, int(bid), "6" + rank_hand_value(hand)])
-    if full_house:
-        ranked_hand.append([hand, int(bid), "5" + rank_hand_value(hand)])
-    if three_of_a_kind:
-        ranked_hand.append([hand, int(bid), "4" + rank_hand_value(hand)])
-    if two_pair:
-        ranked_hand.append([hand, int(bid), "3" + rank_hand_value(hand)])
-    if one_pair:
-        ranked_hand.append([hand, int(bid), "2" + rank_hand_value(hand)])
-    if high_card:
-        ranked_hand.append([hand, int(bid), "1" + rank_hand_value(hand)])
-
-sorted_hands = sorted(ranked_hand, key=lambda x: x[2])
+ranked_hands = sorted(scored_hands, key=lambda x: x[2])
 
 total_winnings = 0
-for i, hand in enumerate(sorted_hands):
+for i, hand in enumerate(ranked_hands):
     total_winnings += hand[1] * (i + 1)
 
-print("Part1 Total Winings are: ", total_winnings)
+print("Part1 Total Winnings are: ", total_winnings)
+
+
+################ Part1 ################
