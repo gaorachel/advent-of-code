da777b077b4e6684e39fbb779d86c2c46ef0f861
Rachel <gggggrea@gmail.com>
2024-07-10 15:54:25 +0100

    Solve day9 part2

--- 2023/day09/solution.py
+++ 2023/day09/solution.py
@@ -19,8 +19,6 @@ for line in lines:
 
     refined_lines.append(int_num_line)
 
-################ Part1 ################
-
 
 def find_sequence_dff(list):
     list_diff = [list[i + 1] - list[i] for i in range(len(list) - 1)]
@@ -28,18 +26,28 @@ def find_sequence_dff(list):
     return list_diff
 
 
+################ Part1 & 2 ################
+
 total_history_value = 0
+total_extrapolated_vale = 0
 for line in refined_lines:
 
     list = line
     matrix_sequence_diff = []
+    prev_sequence_value = None
     while sum(list) != 0:
         list = find_sequence_dff(list)
         matrix_sequence_diff.append(list)
 
     next_sequence_value = sum(row[-1] for row in matrix_sequence_diff)
 
+    prev_sequence_value = 0
+    for row in reversed(matrix_sequence_diff[:-1]):
+        prev_sequence_value = row[0] - prev_sequence_value
+
     total_history_value += line[-1] + next_sequence_value
+    total_extrapolated_vale += line[0] - prev_sequence_value
 
 
 print("Part 1: ", total_history_value)
+print("Part 2: ", total_extrapolated_vale)
