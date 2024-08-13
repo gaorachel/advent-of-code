5a94a524ee85c54280c394330dbf77ca0e72fed2
Rachel <gggggrea@gmail.com>
2024-08-13 22:20:47 +0100

    Solve day11 part1

--- 2023/day11/solution.py
+++ 2023/day11/solution.py
@@ -1,8 +1,7 @@
-import re
-import math
+from itertools import combinations
 
 
-def read_text_file(file_path="dummy.txt"):
+def read_text_file(file_path="input.txt"):
     text_file = open(file_path, "r")
     lines = text_file.readlines()
     text_file.close()
@@ -29,31 +28,38 @@ row_len = len(splitted_lines[0])
 col_len = len(splitted_lines)
 
 empty_space_row_locs = [
-    node for node in range(row_len) if node not in [t[0] for t in galaxy_locations]
+    node for node in range(row_len) if node not in [x for x, _ in galaxy_locations]
 ]
 empty_space_col_locs = [
-    node for node in range(col_len) if node not in [t[1] for t in galaxy_locations]
+    node for node in range(col_len) if node not in [y for _, y in galaxy_locations]
 ]
 
 new_row_len = row_len + len(empty_space_row_locs)
-new_col_len = col_len + len(empty_space_col_locs)
-
 
 matrix = splitted_lines
-
 for row in matrix:
+    additional_cols = 1
     for i in empty_space_col_locs:
-        row.insert(i, ".")
+        row.insert(i + additional_cols, ".")
+        additional_cols += 1
 
+additional_rows = 0
 for i in empty_space_row_locs:
-    matrix.insert(i, ["."] * new_row_len)
-# new_galaxy_locations = []
-# for i, row in enumerate(matrix):
-#     for j, node in enumerate(row):
-#         matrix.insert(matrix[i][j], ["."])
+    matrix.insert(i + additional_rows, ["."] * new_row_len)
+    additional_rows += 1
 
-# # print(1, node)
-# if node == "#":
-#     new_galaxy_locations.append((i, j))
+new_galaxy_locations = []
+for i, row in enumerate(matrix):
+    for j, node in enumerate(row):
+        if node == "#":
+            new_galaxy_locations.append((i, j))
 
-print(matrix)
+pairs = list(combinations(new_galaxy_locations, 2))
+
+total_distance = 0
+i = 1
+for (x1, y1), (x2, y2) in pairs:
+    distance = abs(x2 - x1) + abs(y2 - y1)
+    total_distance += distance
+
+print("Part1: ", total_distance)
