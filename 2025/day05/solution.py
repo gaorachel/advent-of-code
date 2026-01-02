3da982a2a651597c42ff96e592237b6cff771fbc
Rachel <gggggrea@gmail.com>
2026-01-02 23:12:07 +0000

    solve 2025 day5 part2

--- 2025/day05/solution.py
+++ 2025/day05/solution.py
@@ -14,3 +14,17 @@ ids = [int(line) for line in lines[blank_index + 1 :]]
 fresh_ids = [id for id in ids if any(start <= id <= end for start, end in range_ids)]
 
 print("First part answer:", len(fresh_ids))
+
+################ Part1 ################
+range_ids.sort(key=lambda x: x[0])
+
+merged_ranges = []
+for start, end in range_ids:
+    if not merged_ranges or start > merged_ranges[-1][1]:
+        merged_ranges.append([start, end])
+    else:
+        merged_ranges[-1][1] = max(merged_ranges[-1][1], end)
+        print(merged_ranges[-1][1])
+
+total_unique_fresh_id_count = sum(end - start + 1 for start, end in merged_ranges)
+print("Second part answer:", total_unique_fresh_id_count)
