d87f37e37a3d2e0071db6f38581a753678f1fe49
Rachel <gggggrea@gmail.com>
2026-01-02 15:11:30 +0000

    solve 2025 day4 part2

--- 2025/day04/solution.py
+++ 2025/day04/solution.py
@@ -10,7 +10,6 @@ lines = read_text_file()
 
 grid = [list(line.strip()) for line in lines]
 
-################ Part1 ################
 directions = [
     (-1, -1),
     (-1, 0),
@@ -23,7 +22,11 @@ directions = [
 ]
 rows, cols = len(grid), len(grid[0])
 
+
+################ Part1 ################
 x_count = 0
+removed_paper_rolls = 0
+round = 0
 for i in range(rows):
     for j in range(cols):
 
@@ -40,4 +43,35 @@ for i in range(rows):
         if accessible_count < 4:
             x_count += 1
 
-print(x_count)
+print("First answer is ", x_count)
+
+
+################ Part2 ################
+total_removed_count = 0
+while True:
+
+    to_remove = []
+    for i in range(rows):
+        for j in range(cols):
+
+            if grid[i][j] != "@":
+                continue
+
+            accessible_count = 0
+            for dx, dy in directions:
+                ni, nj = i + dx, j + dy
+
+                if 0 <= ni < rows and 0 <= nj < cols and grid[ni][nj] == "@":
+                    accessible_count += 1
+
+            if accessible_count < 4:
+                to_remove.append((i, j))
+
+    if not to_remove:
+        break
+
+    for ri, rj in to_remove:
+        total_removed_count += 1
+        grid[ri][rj] = "."
+
+print("Second answer is ", total_removed_count)
