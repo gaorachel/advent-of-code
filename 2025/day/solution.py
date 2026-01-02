2ee36a94fbeb9ad26eff0072d925993f26ef10b0
Rachel <gggggrea@gmail.com>
2026-01-02 16:21:17 +0000

    update template

--- 2025/day/solution.py
+++ 2025/day/solution.py
@@ -1,13 +1,8 @@
 def read_text_file(file_path="dummy.txt"):
-    text_file = open(file_path, "r")
-    lines = text_file.readlines()
-    text_file.close()
-
-    return lines
+    with open(file_path, "r") as f:
+        return f.read().splitlines()
 
 
 lines = read_text_file()
-
-splitted_lines = [line.split() for line in lines]
 
 ################ Part1 ################
